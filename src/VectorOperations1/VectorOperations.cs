8c323dde53c8d76b4dba42520409f8cf2386d33c
Pau Cabaneros <pau.cabaneros@gmail.com>
2021-10-07 00:59:19 +0200

    remove length variables

--- src/VectorOperations1/VectorOperations.cs
+++ src/VectorOperations1/VectorOperations.cs
@@ -7,9 +7,7 @@ namespace VectorOperations
     {
         public static Vector<T> Append<T>(Vector<T> left, Vector<T> right) where T : struct, IEquatable<T>, IFormattable
         {
-            var leftLength = left.Count;
-            var rightLength = right.Count;
-            Vector<T> vector = Vector<T>.Build.SameAs(left, leftLength + rightLength);
+            Vector<T> vector = Vector<T>.Build.SameAs(left, left.Count + right.Count);
             left.Storage.CopySubVectorTo(vector.Storage, 0, 0, left.Count, ExistingData.AssumeZeros);
             right.Storage.CopySubVectorTo(vector.Storage, 0, left.Count, right.Count, ExistingData.AssumeZeros);
             return vector;
