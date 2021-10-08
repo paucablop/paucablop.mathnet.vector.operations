6ea4c822331830dac2c519e1a029bb880150ec31
Pau Cabaneros <pau.cabaneros@gmail.com>
2021-10-08 23:07:39 +0200

    fix append test

--- test/UnitTests/UnitTest1.cs
+++ test/UnitTests/UnitTest1.cs
@@ -1,3 +1,5 @@
+using System;
+using FluentAssertions;
 using MathNet.Numerics.LinearAlgebra;
 using Xunit;
 
@@ -6,7 +8,7 @@ namespace UnitTests
     public class UnitTest1
     {
         [Fact]
-        public void TestAppend()
+        public void TestAppendShouldAppend()
         {
             // Arrange
             Vector<double> leftVector = Vector<double>.Build.Dense(5, 1.0);
@@ -16,9 +18,23 @@ namespace UnitTests
             var vector = VectorOperations.VectorOperations.Append(leftVector, righVector);
 
             // Assert
+            Assert.IsAssignableFrom<Vector<double>>(vector);
             Assert.Equal(9, vector.Count);
             Assert.Equal(1.0, vector[0]);
             Assert.Equal(2.0, vector[5]);
+        }
+        [Fact]
+        public void TestAppendThrowException()
+        {
+            // Arrange
+            Vector<double> leftVector = null;
+            Vector<double> righVector = Vector<double>.Build.Dense(4, 2.0);
+
+            // Act
+            var vector = VectorOperations.VectorOperations.Append(leftVector, righVector);
+
+            // Assert
+            vector.Should().Thr
         }
     }
 }
